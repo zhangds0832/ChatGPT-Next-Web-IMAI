855924a8b3b2e5c4c6ae4ea442516d63c25c3d31
zds <664256297@qq.com>
2024-10-10 17:06:10 +0800

    add 增加打包脚本

---
+++ update.sh
@@ -0,0 +1,9 @@
+tar -czvf dist.tar.gz -C out .
+scp -P 27652 dist.tar.gz root@66.112.217.45:/var/www/ng-cf/html/ai-h5
+ssh root@66.112.217.45 -p 27652 "cd  /var/www/ng-cf/html/ai-h5; tar -xzvf dist.tar.gz; rm dist.tar.gz; exit"
+rm dist.tar.gz
+
+
+# chatgpt
+# 66.112.217.45 -p 27652
+
